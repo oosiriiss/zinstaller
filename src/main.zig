cba9bafc9012b2ea4288e3ac5e94adae387cb500
oosiriiss <oosiriiss@gmail.com>
2025-01-16 19:17:58 +0100

    da

--- src/main.zig
+++ src/main.zig
@@ -45,9 +45,6 @@ fn flattenPackagesArray(packages: []const Package) !std.ArrayList(Package) {
     var flattenedArr = std.ArrayList(Package).init(std.heap.page_allocator);
 
     for (packages) |package| {
-        if (!exists(&flattenedArr, package))
-            try flattenedArr.append(package);
-
         if (package.dependencies) |deps| {
             for (deps) |dependency| {
                 // Dependency is not added
@@ -56,12 +53,36 @@ fn flattenPackagesArray(packages: []const Package) !std.ArrayList(Package) {
                 }
             }
         }
+
+        if (!exists(&flattenedArr, package))
+            try flattenedArr.append(package);
     }
 
     return flattenedArr;
 }
 
+fn extractPackageNames(packages: *const std.ArrayList(Package)) ![]const u8 {
+    var len = packages.items.len; // space for spaces
+    for (packages.items) |package| {
+        len += package.name.len;
+    }
+
+    var str = try std.heap.page_allocator.alloc(u8, len);
+
+    var i: usize = 0;
+    for (packages.items) |package| {
+        for (package.name) |c| {
+            str[i] = c;
+            i = i + 1;
+        }
+        str[i] = ' ';
+        i = i + 1;
+    }
+    return str;
+}
+
 pub fn main() !void {
+    const stdout = std.io.getStdOut().writer();
 
     // List of packages
     const packages = comptime [_]Package{
@@ -76,14 +97,44 @@ pub fn main() !void {
             Package.init("qt6-wayland", "Qt6 with wayland support"),
         }),
     };
+
+    // Selecting packages
+
     // Flatteing packages list (adding dependencies to the main list)
     const flattened = flattenPackagesArray(&packages) catch |err| {
-        std.debug.print("Couldn't flatten packages array: {}\n", .{err});
+        std.debug.print("Couldn't flatten packages array: {any}\n", .{err});
         return;
     };
     defer flattened.deinit();
+
     try dumpPackagesToFile(&flattened);
 
-    const proc = try std.process.Child.run(.{ .allocator = std.heap.page_allocator, .argv = &.{"dir.exe"}, .cwd_dir = std.fs.cwd() });
-    _ = proc;
+    try stdout.print("Synchronizing pacman\n", .{});
+    var syncResult = std.process.Child.init(&.{ "pacman", "-Syu", "--noconfirm" }, std.heap.page_allocator);
+
+    _ = syncResult.spawnAndWait() catch |err| {
+        std.debug.print("Couldn't pacman -Syu: {any}", .{err});
+        return;
+    };
+
+    syncResult.stdout_behavior = std.process.Child.StdIo.Pipe;
+    syncResult.stdin_behavior = std.process.Child.StdIo.Pipe;
+
+    //std.debug.print("Synchronization Result: {d}\nSynchronization stdout: {s}\nSynchronization stderr: {s}", .{ syncResult.term.Exited, syncResult.stdout, syncResult.stderr });
+
+    const packagesStr = extractPackageNames(&flattened) catch |err| {
+        std.debug.print("There was an error when extracting Packagae names: {any}", .{err});
+        return;
+    };
+
+    defer std.heap.page_allocator.free(packagesStr);
+
+    const command = [_][]const u8{ "pacman", "-S", packagesStr };
+
+    std.debug.print("Downloading packages: {s}", .{packagesStr});
+    const installResult = std.process.Child.run(.{ .allocator = std.heap.page_allocator, .argv = &command }) catch |err| {
+        std.debug.print("Couldn't pacman -Syu: {any}", .{err});
+        return;
+    };
+    std.debug.print("Installation stdout: {s}\nInstallation stderr: {s}", .{ installResult.stdout, installResult.stderr });
 }
