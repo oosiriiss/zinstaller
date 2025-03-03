70509d456e894cd39a673b21ca21f2ca924e6188
Oosiriiss <oosiriiss@gmail.com>
2025-03-03 23:55:03 +0100

    da

--- installer/src/selecting_packages.zig
+++ installer/src/selecting_packages.zig
@@ -14,20 +14,36 @@ const InputTokenType = enum {
 
 const InputToken = union(InputTokenType) { number: u32, range: Range };
 
-fn askForPackages() std.ArrayList(InputToken) {
+
+
+
+pub fn selectPackages() {
+
+//
+        util.print("Please numbers or ranges of desired packages (i.e \"1 2 3 5-8 19 72 420 13\")\n", .{});
+        util.print(">>> ", .{});
+}
+
+
+
+
+
+
+fn askForPackages() std.mem.Allocator.Error!std.ArrayList(InputToken) {
     const stdin = std.io.getStdIn().reader();
 
     while (true) {
-        util.print("Please numbers or ranges of desired packages (i.e \"1 2 3 5-8 19 72 420 13\")\n", .{});
-        util.print(">>> ", .{});
 
         if (stdin.readUntilDelimiterAlloc(std.heap.page_allocator, '\n', 4096)) |input| {
             defer std.heap.page_allocator.free(input);
 
-            //
-            parseSelectionInput(input);
-            //
+            const tokens = try parseSelectionInput(input);
+            if (tokens.items.len <= 0) {
+                tokens.deinit();
+                std.debug.print("Please insert valid numbers\n", .{});
+            }
 
+            return tokens;
         } else |err| {
             switch (err) {
                 error.StreamTooLong => {
@@ -44,8 +60,8 @@ fn askForPackages() std.ArrayList(InputToken) {
     }
 }
 
-fn parseSelectionInput(input: []const u8) !std.ArrayList(InputToken) {
-    const tokens = std.ArrayList(InputToken);
+fn parseSelectionInput(input: []const u8) std.mem.Allocator.Error!std.ArrayList(InputToken) {
+    const tokens = std.ArrayList(InputToken).init(std.heap.page_allocator);
 
     const tokenizer = std.mem.tokenizeScalar(u8, input, ' ');
 
@@ -77,17 +93,60 @@ fn parseNumberToken(str: []const u8) !InputToken {
     return std.fmt.parseInt(u32, str, 10);
 }
 
-fn parseRangeToken(str: []const u8) !InputToken {
-    const splitIter = std.mem.splitScalar(str, '-');
+fn parseRangeToken(str: []const u8) error{InvalidRange}!InputToken {
+    var splitIter = std.mem.splitScalar(u8, str, '-');
 
-    const bottom = splitIter.next() orelse return error.ParseError;
-    const top = splitIter.next() orelse return error.ParseError;
+    const startToken = splitIter.next() orelse return error.InvalidRange;
+    const endToken = splitIter.next() orelse return error.InvalidRange;
 
-    try std.fmt.parseInt(bottom);
-    try std.fmt.parseInt(top);
+    const start = std.fmt.parseInt(u32, startToken, 10) catch return error.InvalidRange;
+    const end = std.fmt.parseInt(u32, endToken, 10) catch return error.InvalidRange;
 
     if (splitIter.next() != null)
-        return false;
+        return error.InvalidRange;
 
-    return InputToken{ .range = Range{ .start = bottom, .top = top } };
+    if (start > end)
+        return error.InvalidRange;
+
+    return InputToken{ .range = Range{ .start = start, .end = end } };
+}
+
+test "Parsing valid range" {
+    const res = try parseRangeToken("5-12");
+    try std.testing.expect(res.range.start == 5 and res.range.end == 12);
+
+    const res2 = try parseRangeToken("0-15");
+    try std.testing.expect(res2.range.start == 0 and res2.range.end == 15);
+
+    const res3 = try parseRangeToken("8-122");
+    try std.testing.expect(res3.range.start == 8 and res3.range.end == 122);
+}
+
+test "range start higher than top should throw" {
+    const res = parseRangeToken("12-9");
+    try std.testing.expectError(error.InvalidRange, res);
+}
+
+test "Too much components of range OR negative numbers in range" {
+    const res3 = parseRangeToken("12-125-99");
+    try std.testing.expectError(error.InvalidRange, res3);
+}
+
+test "Non numerical value in a range" {
+    const res = parseRangeToken("15-asdasdeleven");
+    try std.testing.expectError(error.InvalidRange, res);
+
+    const res2 = parseRangeToken("dasda-15");
+    try std.testing.expectError(error.InvalidRange, res2);
+
+    const res3 = parseRangeToken("asd-asdasdeleven");
+    try std.testing.expectError(error.InvalidRange, res3);
+}
+
+test "Not enough components in range" {
+    const res = parseRangeToken("-xd");
+    try std.testing.expectError(error.InvalidRange, res);
+
+    const res2 = parseRangeToken("asd-");
+    try std.testing.expectError(error.InvalidRange, res2);
 }
