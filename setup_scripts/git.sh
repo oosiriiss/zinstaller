229f2bb642f7b4a2b4742e634cc7fb1455aa27ee
oosiriiss <oosiriiss@gmail.com>
2025-08-13 16:06:36 +0200

    Git setup script

---
+++ setup_scripts/git.sh
@@ -0,0 +1,14 @@
+
+
+NAME="oosiriiss"
+EMAIL="oosiriss@gmail.com"
+EDITOR="nvim"
+
+echo "Settings git user.name to $NAME"
+git config --global user.name "$NAME"
+
+echo "Setting git user.email to $EMAIL"
+git config --global user.email "$EMAIL"
+
+echo "Setting it core.editor to $EDITOR"
+git config --global core.editor "$EDITOR"
