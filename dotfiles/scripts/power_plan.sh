da2e4ca82cdf9a53b0df3e083410b1d7f661ea5e
oosiriiss <oosiriiss@gmail.com>
2025-10-08 21:11:38 +0200

    Readme and power plan script update

--- dotfiles/scripts/power_plan.sh
+++ dotfiles/scripts/power_plan.sh
@@ -1,35 +1,46 @@
 #!/bin/bash
 
-get_current_power_plan() {
-	currentplan=$(asusctl profile -p | tail -1 | awk '{print $NF}')
-}
+currentplan=$(powerprofilesctl get)
 
 # If User is switching mode switch it before sending info to waybar
 if [[ "$1" == "next" ]]; then
-	asusctl profile -n
-fi
 
-get_current_power_plan
+   profiles=(power-saver balanced performance)
+
+   # Finding index 
+   for i in "${!profiles[@]}"; do
+      if [[ "${profiles[$i]}" == "$currentplan" ]]; then
+	 index=$i
+	 break
+      fi
+   done
+
+   next_index=$(( (index + 1) % ${#profiles[@]} ))
+      next="${profiles[$next_index]}"
+   
+   powerprofilesctl set "$next"
+
+fi
 
 # MAIN monitor.. idk how this would work with multiple monitors
 MONITOR=$(hyprctl monitors | grep "Monitor" | cut --delimiter=" " -f 2)
 
-# disableing echo
+# disabling echo
 stty -echo;
-if [ "$currentplan" == "Balanced" ]; then
+if [ "$currentplan" == "balanced" ]; then
 	text=""
 	tooltip="Balanced"
 	class="balanced"
 	hyprctl keyword monitor $MONITOR,1920x1080@144.06,0x0,1  >> ~/.config/waybar/log.txt;
-elif [ "$currentplan" == "Performance" ]; then
+elif [ "$currentplan" == "performance" ]; then
 	text=""
 	tooltip="Performance"
 	class="performance"
 	hyprctl keyword monitor $MONITOR,1920x1080@144.06,0x0,1 >> ~/.config/waybar/log.txt;
-elif [ "$currentplan" == "Quiet" ]; then
+elif [ "$currentplan" == "power-saver" ]; then
 	text=""
-	tooltip="Quiet"
-	class="quiet"
+	tooltip="Power saver"
+	class="power-saver"
 	hyprctl keyword monitor $MONITOR,1920x1080@60.06,0x0,1 >> ~/.config/waybar/log.txt;
 fi
 
