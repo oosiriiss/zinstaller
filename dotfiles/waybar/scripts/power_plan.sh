097fb59982a9da86a2cc1b2fa4ef148abc1c1e44
oosiriiss <oosiriiss@gmail.com>
2025-10-09 20:20:00 +0200

    Start of waybar revamp

--- dotfiles/waybar/scripts/power_plan.sh
+++ dotfiles/waybar/scripts/power_plan.sh
@@ -22,24 +22,22 @@ if [[ "$1" == "next" ]]; then
 
 fi
 
+currentplan=$(powerprofilesctl get)
+
 # MAIN monitor.. idk how this would work with multiple monitors
 MONITOR=$(hyprctl monitors | grep "Monitor" | cut --delimiter=" " -f 2)
 
 # disabling echo
-stty -echo;
 if [ "$currentplan" == "balanced" ]; then
-	text=""
-	tooltip="Balanced"
+	planname="Balanced"
 	class="balanced"
 	hyprctl keyword monitor $MONITOR,1920x1080@144.06,0x0,1  >> ~/.config/waybar/log.txt;
 elif [ "$currentplan" == "performance" ]; then
-	text=""
-	tooltip="Performance"
+	planname="Performance"
 	class="performance"
 	hyprctl keyword monitor $MONITOR,1920x1080@144.06,0x0,1 >> ~/.config/waybar/log.txt;
 elif [ "$currentplan" == "power-saver" ]; then
-	text=""
-	tooltip="Power saver"
+	planname="Power saver"
 	class="power-saver"
 	hyprctl keyword monitor $MONITOR,1920x1080@60.06,0x0,1 >> ~/.config/waybar/log.txt;
 fi
@@ -49,5 +47,8 @@ if [[ "$1" == "next" ]]; then
 	notify-send -h string:x-canonical-private-synchronous:sys-notify -u low "Power Profile: $currentplan"
 fi
 #Enablig echo
-stty echo;
-echo '{"text": "'$text'", "tooltip": "'Power plan: $tooltip'", "class": "'$class'"}'
+
+readonly OUTPUT="{\"text\":\"$planname\", \"alt\": \"$class\", \"tooltip\": \"Power plan: $planname\", \"class\": \"$class\"}"
+
+echo "$OUTPUT" >> ~/.config/waybar/log.txt
+echo "$OUTPUT";
